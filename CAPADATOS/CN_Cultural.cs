96480d0dd2ff4fea02c984ea20bdc86db407ae06
Angy2025 <angieriverapierre@gmail.com>
2025-06-19 00:19:01 -0400

    Nuevo cambio

---
+++ CAPADATOS/CN_Cultural.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPA_DE_NEGOCIOS
+{
+    public class Cultural : EventoBase
+    {
+        public Cultural()
+        {
+            Tipo = "Cultural";
+        }
+
+        public override string ObtenerDetalles()
+        {
+            string resumenBase = base.ObtenerResumenEvento();
+
+            return $"{resumenBase} Este evento cultural se llevara a cabo el {ObtenerFechaBien()} y tendra una capacidad de {Capacidad} personas.;\r\n";
+        }
+    }
+}
---
+++ CAPADATOS/CN_EventoBase.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPA_DE_NEGOCIOS
+{
+    public abstract class EventoBase
+    {
+        //Atributos de la clase eventos para ser heredados en otras clases
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; } 
+        public string Nombre { get; set; }
+        public string Lugar { get; set; }
+        public string Tipo { get; set; }
+        public int Capacidad { get; set; } //Capacidad del evento
+
+
+        public virtual string ObtenerResumenEvento()
+        {
+            return $"El evento '{Nombre}' se realizara en {Lugar}.";
+        }
+
+        public abstract string ObtenerDetalles();
+
+
+        public string ObtenerFechaBien()
+        {
+            return Fecha.ToString("d 'de' MMM 'del' yyyy 'a las' hh:mm tt");
+        }
+    }
+}
